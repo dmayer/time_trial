da1b57c7f3f6a2b7b759d62d85011e046c4a1c35
Daniel A. Mayer <daniel@matasano.com>
2014-07-23 10:50:37 -0500

    Fixing relative paths for driver to binaries

--- time_trial_gui/lib/racer_driver.py
+++ time_trial_gui/lib/racer_driver.py
@@ -4,8 +4,8 @@ import os
 
 __author__ = 'daniel'
 
-CPP_HTTP_TIMING_EXECUTABLE = '../../../../cpp/bin/run_http_timing_client'
-CPP_ECHO_TIMING_EXECUTABLE = "/home/mayer/repos/remote_timing_attacks/src/cpp/bin/run_timing_client"
+CPP_HTTP_TIMING_EXECUTABLE = '../../racer/bin/run_http_timing_client'
+CPP_ECHO_TIMING_EXECUTABLE = "../../racer/bin/run_timing_client"
 
 
 import subprocess
